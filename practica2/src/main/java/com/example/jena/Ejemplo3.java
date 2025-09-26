2d66b2525f60340fc057df37404e938c202a03e7
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-09-26 11:30:55 +0200

    Other initial file

---
+++ practica2/src/main/java/com/example/jena/Ejemplo3.java
@@ -0,0 +1,20 @@
+package com.example.jena;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.RDFDataMgr;
+
+public class Ejemplo3 {
+    //static final String inputFileName  = "practica2/vc-db-1.rdf";
+    static final String inputFileName  = "practica2/personas.ttl";
+                              
+    public static void main (String args[]) {
+        // create an empty model
+        Model model = ModelFactory.createDefaultModel();
+        // Leer el archivo directamente en el modelo
+        RDFDataMgr.read(model, inputFileName);
+                    
+        // write it to standard out
+        model.write(System.out);            
+    }
+}
