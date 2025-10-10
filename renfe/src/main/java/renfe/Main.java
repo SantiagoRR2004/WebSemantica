c4914ff1fb034e360288a4a215ecb2f376ce46e4
sergioooo0 <sergiogonzalezrodriguez05@gmail.com>
2025-10-10 12:11:28 +0200

    read stops.txt

--- renfe/src/main/java/renfe/Main.java
+++ renfe/src/main/java/renfe/Main.java
@@ -1,7 +1,19 @@
 package renfe;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String txt_path = "data/stops.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(txt_path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
