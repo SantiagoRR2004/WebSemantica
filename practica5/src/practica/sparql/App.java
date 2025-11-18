b2b5678beca009d75a8d8bedb2dd9d8fb961dfa9
GitHub Action <action@github.com>
2025-11-18 10:09:18 +0000

    chore: Auto-format Java code [GitHub Action]

--- practica5/src/practica/sparql/App.java
+++ practica5/src/practica/sparql/App.java
@@ -1,5 +1,5 @@
-public class App {   
-    public static void main(String[] args) {
-        System.out.println("Hello, SPARQL World!");
-    }
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello, SPARQL World!");
+  }
 }
