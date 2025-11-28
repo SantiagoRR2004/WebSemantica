adefc2ec301b2c89ecf308d67aaa9ffd99877872
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-11-28 10:56:05 +0100

    Use CONSTRUCT and new file

--- practica6/src/main/java/Main.java
+++ practica6/src/main/java/Main.java
@@ -1,13 +1,5 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import org.apache.jena.query.Query;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.QueryExecutionFactory;
-import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.ResultSetFormatter;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.riot.RDFDataMgr;
 
 public class Main {
 
@@ -17,25 +9,15 @@ public class Main {
       Paths.get(System.getProperty("user.dir"), "queries", "q1.sparql").toString();
 
   public static void main(String[] args) {
-    // Create an empty model
-    Model model = ModelFactory.createDefaultModel();
-
-    // Open the file
-    RDFDataMgr.read(model, inputFileName);
-
     String queryString = "";
-
     try {
       queryString = Files.readString(Paths.get(queryFile));
     } catch (Exception e) {
       System.err.println("Error reading " + queryFile + ": " + e.getMessage());
+      return;
     }
 
-    System.out.println(queryString);
-
-    Query query = QueryFactory.create(queryString);
-    try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
-      ResultSetFormatter.out(System.out, qexec.execSelect(), query);
-    }
+    SparqlRunner runner = new SparqlRunner(inputFileName);
+    runner.runConstructQuery(queryString);
   }
 }
---
+++ practica6/src/main/java/SparqlRunner.java
@@ -0,0 +1,24 @@
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+public class SparqlRunner {
+  private Model model;
+
+  public SparqlRunner(String rdfFilePath) {
+    model = ModelFactory.createDefaultModel();
+    model.read(rdfFilePath);
+  }
+
+  public void runConstructQuery(String queryStr) {
+    Query query = QueryFactory.create(queryStr);
+    try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
+      Model constructModel = qexec.execConstruct();
+      constructModel.write(System.out, "TURTLE");
+    }
+  }
+
+}
