ec0dd2400a63032c3f0fe10bd159ec6dde6d6c7f
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-12-15 20:58:49 +0100

    Add university

---
+++ university/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.nio.file.Paths;
+
+public class Main {
+
+    static final String inputFileName = Paths.get(System.getProperty("user.dir"), "university.ttl").toString();
+
+    public static void main(String[] args) {
+
+    }
+}
